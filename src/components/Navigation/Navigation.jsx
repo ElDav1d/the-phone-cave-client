a0f7de71a8b94dbd4165ac23a2cc3fc5f7204d1b
ElDav1d <dvivoes@yahoo.es>
2023-03-30 18:06:42 +0200

    apply routing

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import Navigation from "../components/Navigation";
+
+function Home() {
+  return (
+    <>
+      <Navigation />
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default Home;
