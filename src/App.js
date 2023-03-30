e2598ddabf7312e0420331ac3b05fe3e22fb1481
ElDav1d <dvivoes@yahoo.es>
2023-03-30 18:22:14 +0200

    apply error handling routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Error from "./pages/errors/Error";
+import NotFound from "./pages/errors/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
